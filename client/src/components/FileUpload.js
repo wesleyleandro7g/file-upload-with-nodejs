edc0772505d79da88cb3e9e1fbcfaacf241e1958
wesleyleandrosantos14 <wesleyleandrosantos14@gmail.com>
2020-06-23 12:31:20 -0300

    Testing upload

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
+import Axios from "axios";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [fileName, setFileName] = useState("Selecione o arquivo");
+  const [uploadedFile, setUploadedFile] = useState({});
 
   const onChange = (e) => {
     setFile(e.target.files[0]);
     setFileName(e.target.files[0].name);
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
     const formData = new FormData();
 
     formData.append("file", file);
+
+    try {
+      const res = await Axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log("There was problem with the server");
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
   };
 
   return (
