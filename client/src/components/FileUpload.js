a5b3385b33137f88aa1867f127abfae0d70b7434
wesleyleandrosantos14 <wesleyleandrosantos14@gmail.com>
2020-06-23 16:20:15 -0300

    Implement progressBar and message

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import Message from "./Message";
+import ProgressBar from "./Progress";
 import Axios from "axios";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [fileName, setFileName] = useState("Selecione o arquivo");
   const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState("");
+  const [uploadPercentage, setUploadPercentage] = useState(0);
 
   const onChange = (e) => {
     setFile(e.target.files[0]);
@@ -23,22 +27,38 @@ const FileUpload = () => {
         headers: {
           "Content-Type": "multipart/form-data",
         },
+        onUploadProgress: (ProgressEvent) => {
+          setUploadPercentage(
+            parseInt(
+              Math.round((ProgressEvent.loaded * 100) / ProgressEvent.total)
+            )
+          );
+
+          setTimeout(() => {
+            setUploadPercentage(0);
+            setMessage("");
+            setUploadedFile({});
+            setFileName("Selecione outro arquivo");
+          }, 15000);
+        },
       });
 
       const { fileName, filePath } = res.data;
 
       setUploadedFile({ fileName, filePath });
+      setMessage("File Uploaded");
     } catch (err) {
       if (err.response.status === 500) {
-        console.log("There was problem with the server");
+        setMessage("There was problem with the server");
       } else {
-        console.log(err.response.data.msg);
+        setMessage(err.response.data.msg);
       }
     }
   };
 
   return (
     <>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className="custom-file">
           <input
@@ -52,12 +72,23 @@ const FileUpload = () => {
           </label>
         </div>
 
+        <ProgressBar percentage={uploadPercentage} />
+
         <input
           type="submit"
           value="Upload"
           className="btn btn-primary btn-block mt-4"
         />
       </form>
+
+      {uploadedFile ? (
+        <div className="row mt-5">
+          <div className="col-md-6 m-auto">
+            <h3 className="text-center"> {uploadedFile.fileName} </h3>
+            <img style={{ width: "100%" }} src={uploadedFile.filePath} />
+          </div>
+        </div>
+      ) : null}
     </>
   );
 };
